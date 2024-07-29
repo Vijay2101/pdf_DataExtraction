c578588ae78aa72b303a523730216bc1ff25698e
Vijay Kumar <kumarvijay2003.vk@gmail.com>
2024-07-29 23:40:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from application import app
 
 
-import os
 
 
-if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True,port=3000)
+
